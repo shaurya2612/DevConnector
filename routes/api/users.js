9fb614186b475fc60a42292b7b3b53996bd4ab5a
mac <mac@macs-MacBook-Air.local>
2020-12-06 18:45:14 +0530

    modified route desc

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,7 +9,7 @@ const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
 
 //@route    POST api/users
-//@desc     Test Route
+//@desc     Register user
 //@access   Public
 router.post(
   "/",
